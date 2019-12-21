b5ef09de3d0793f1fda7f656d73ec6c15316924d
SWalls <SWalls@users.noreply.github.com>
2019-12-21 13:00:17 -0800

    Add writePoem function with rhyming words and sentence APIs to generate a sentence.

---
+++ index.js
@@ -0,0 +1,89 @@
+const https = require('https');
+
+const subjects = [
+  'earth','climate','gender',
+  'jealousy','health','mindfulness',
+  'treason','democracy','serenity',
+  'love','hate','sex','crime',
+  'death',
+  'banana'
+];
+
+function getRandomBetween(i, j) {
+  return Math.floor(Math.random() * j) + i;
+}
+
+function getRandomSubject() {
+  return subjects[getRandomBetween(0, subjects.length)];
+}
+
+function httpGet(api) {
+  console.log(`GET: ${api}`);
+  return new Promise((resolve, reject) => {
+    https.get(api, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        const result = JSON.parse(data);
+        resolve(result);
+      });
+    }).on("error", (err) => {
+      console.log("HTTP Error: " + err.message);
+      reject(err);
+    });
+  });
+}
+
+function sendDatamuseRhymeRequest(word) {
+  return httpGet(`https://api.datamuse.com/words?rel_rhy=${word}`);
+}
+
+function getRhymingWords(word) {
+  return new Promise((resolve) => {
+    sendDatamuseRhymeRequest(word).then((words) => {
+      resolve(words);
+    }).catch(() => {
+      resolve([]);
+    });
+  });
+}
+
+function sendLinguatoolsSentenceRequest(options) {
+  let url = `https://lt-nlgservice.herokuapp.com/rest/english/realise?subject=${options.subject}`;
+  if (typeof options.object === 'array' && options.object.length > 0) {
+    options.object.forEach((obj) => url += `&object=${obj}`);
+  }
+  return httpGet(url);
+}
+
+function getSentence(options) {
+  return new Promise((resolve) => {
+    sendLinguatoolsSentenceRequest(options).then((sentence) => {
+      resolve(sentence);
+    }).catch(() => {
+      resolve('No sentence for: ' + options);
+    });
+  });
+}
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.writePoem = (req, res) => {
+  const word = req.query.word || req.body.word || 'Yang';
+  const subject = getRandomSubject();
+  console.log(`Chosen subject: ${subject}`);
+  getRhymingWords(word).then((rhymingWords) => {
+  	console.log(`Found ${rhymingWords.length} words that rhyme with ${word}.`);
+    const objects = rhymingWords.map((w) => w.word).slice(0,3);
+    getSentence({'subject': subject, 'object': objects}).then((sentence) => {
+      res.status(200).send(sentence);
+    });
+  });
+};
+
